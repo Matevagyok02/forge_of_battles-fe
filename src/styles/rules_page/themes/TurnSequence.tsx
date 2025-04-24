9241129d616271e1d3d24a29ec0f4be552eae5df
Matevagyok02 <mr.szaszak@gmail.com>
2025-04-24 14:51:28 +0200

    updated DecksAndCards styles

--- src/styles/rules_page/themes/TurnSequence.tsx
+++ src/styles/rules_page/themes/TurnSequence.tsx
@@ -2,34 +2,39 @@ import styles from "../../../styles/rules_page/Rules.module.css";
 const TurnSequence = () => {
 
     return (
-        <ul className={styles.turnSequence} >
-            <li>
-                <h1>1. Draw</h1>
-                <p>
-                    is a deeply strategic, two-player card game where calculated moves and careful planning determine the outcome of a relentless war of attrition. As a battlefield commander, you must lead your forces with foresight and precision, aiming to wear down your opponent and outlast their defenses.
-                    Each card in your deck is a versatile resource, offering multiple tactical options. Every decision—whether to attack, defend, or sacrifice—comes with weight and consequence. Success demands not just boldness, but the ability to anticipate and adapt to your opponent’s strategy.
-                    In the Forge of Battles, victory doesn’t go to the quickest hand, but to the sharpest mind.
-                </p>
-            </li>
-            <vertical-line/>
-            <li>
-                <h1>2. Advance and Engage</h1>
-                <p>
-                    is a deeply strategic, two-player card game where calculated moves and careful planning determine the outcome of a relentless war of attrition. As a battlefield commander, you must lead your forces with foresight and precision, aiming to wear down your opponent and outlast their defenses.
-                    Each card in your deck is a versatile resource, offering multiple tactical options. Every decision—whether to attack, defend, or sacrifice—comes with weight and consequence. Success demands not just boldness, but the ability to anticipate and adapt to your opponent’s strategy.
-                    In the Forge of Battles, victory doesn’t go to the quickest hand, but to the sharpest mind.
-                </p>
-            </li>
-            <vertical-line/>
-            <li>
-                <h1>3. Play Cards</h1>
-                <p>
-                    is a deeply strategic, two-player card game where calculated moves and careful planning determine the outcome of a relentless war of attrition. As a battlefield commander, you must lead your forces with foresight and precision, aiming to wear down your opponent and outlast their defenses.
-                    Each card in your deck is a versatile resource, offering multiple tactical options. Every decision—whether to attack, defend, or sacrifice—comes with weight and consequence. Success demands not just boldness, but the ability to anticipate and adapt to your opponent’s strategy.
-                    In the Forge of Battles, victory doesn’t go to the quickest hand, but to the sharpest mind.
-                </p>
-            </li>
-        </ul>
+        <div className={styles.turnSequence} >
+            <p>
+                Each turn in Forge of Battles has 3 main phases. Here’s how they work:
+            </p>
+            <ul>
+                <li>
+                    <h1>1. Draw</h1>
+                    <p>
+                        is a deeply strategic, two-player card game where calculated moves and careful planning determine the outcome of a relentless war of attrition. As a battlefield commander, you must lead your forces with foresight and precision, aiming to wear down your opponent and outlast their defenses.
+                        Each card in your deck is a versatile resource, offering multiple tactical options. Every decision—whether to attack, defend, or sacrifice—comes with weight and consequence. Success demands not just boldness, but the ability to anticipate and adapt to your opponent’s strategy.
+                        In the Forge of Battles, victory doesn’t go to the quickest hand, but to the sharpest mind.
+                    </p>
+                </li>
+                <vertical-line/>
+                <li>
+                    <h1>2. Advance and Engage</h1>
+                    <p>
+                        is a deeply strategic, two-player card game where calculated moves and careful planning determine the outcome of a relentless war of attrition. As a battlefield commander, you must lead your forces with foresight and precision, aiming to wear down your opponent and outlast their defenses.
+                        Each card in your deck is a versatile resource, offering multiple tactical options. Every decision—whether to attack, defend, or sacrifice—comes with weight and consequence. Success demands not just boldness, but the ability to anticipate and adapt to your opponent’s strategy.
+                        In the Forge of Battles, victory doesn’t go to the quickest hand, but to the sharpest mind.
+                    </p>
+                </li>
+                <vertical-line/>
+                <li>
+                    <h1>3. Play Cards</h1>
+                    <p>
+                        is a deeply strategic, two-player card game where calculated moves and careful planning determine the outcome of a relentless war of attrition. As a battlefield commander, you must lead your forces with foresight and precision, aiming to wear down your opponent and outlast their defenses.
+                        Each card in your deck is a versatile resource, offering multiple tactical options. Every decision—whether to attack, defend, or sacrifice—comes with weight and consequence. Success demands not just boldness, but the ability to anticipate and adapt to your opponent’s strategy.
+                        In the Forge of Battles, victory doesn’t go to the quickest hand, but to the sharpest mind.
+                    </p>
+                </li>
+            </ul>
+        </div>
     );
 }
 
